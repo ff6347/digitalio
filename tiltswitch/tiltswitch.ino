956b1feb7ca4dfad2f9b9dd481b376b4a1c20a48
fabiantheblind <icke@fabianmoronzirfas.me>
2014-04-26 09:10:43 +0200

    Added graph: new file:   IC_74HC595_Graph/74hc595-chart/graph-74HC595.dot new file:   IC_74HC595_Graph/74hc595-chart/graph.png
    minor changes:
    modified:   IC_74HC595_Graph/IC_74HC595_Graph.fzz
    modified:   tiltswitch/tiltswitch.ino

--- tiltswitch/tiltswitch.ino
+++ tiltswitch/tiltswitch.ino
@@ -6,12 +6,13 @@
 int ledpin = 13; // for output
 int tiltpin = 2; // tiltswitch in put
 int is_on = 0;// boolean
+int [] pins = [13, 12, 11, 10];
 
 void setup(){
 pinMode(ledpin, OUTPUT); // init
-pinMode(tiltpin, INPUT); // inti
-  
+pinMode(tiltpin, INPUT); // init
 }
+
 void loop(){
   is_on = digitalRead(tiltpin); // read from the switch
 
